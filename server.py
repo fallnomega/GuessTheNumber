6fc49827b3c0f938df573d0b6511473e22efa864
fallnomega <fallnomega@gmail.com>
2023-03-28 13:42:57 -0700

    init commit

---
+++ server.py
@@ -0,0 +1,10 @@
+# Final Project - Higher or Lower URLs Now it's time to complete the final project of the day, the higher lower game
+# that we created in Day 14, but now with a real website.
+#
+# 1. Create a new project in PyCharm called higher-lower, add a server.py file.
+#
+# 2. Create a new Flask application where the home route displays an <h1> that says "Guess a number between 0 and 9"
+# and display a gif of your choice from giphy.com.
+#
+# Alternatively use the one I found on Giphy: https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif
+#
